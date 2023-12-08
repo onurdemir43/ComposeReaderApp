350efb409115152ad869d08aa96f7a8ae46cffce
onurdemir43 <142926729+onurdemir43@users.noreply.github.com>
2023-12-08 17:31:33 +0300

    Setting up Splash Screen

--- ComposeReaderApp/app/src/main/java/com/onurdemir/composereaderapp/screens/ReaderSplashScreen.kt
+++ ComposeReaderApp/app/src/main/java/com/onurdemir/composereaderapp/screens/ReaderSplashScreen.kt
@@ -1,11 +1,74 @@
 package com.onurdemir.composereaderapp.screens
 
+import android.view.animation.OvershootInterpolator
+import androidx.compose.animation.core.Animatable
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.scale
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
-
+import com.onurdemir.composereaderapp.navigation.ReaderScreens
+import kotlinx.coroutines.delay
 
 @Composable
 fun ReaderSplashScreen(navController: NavController) {
-    Text(text = "SplashScreen")
+
+    val scale = remember {
+        Animatable(0f)
+    }
+
+    LaunchedEffect(key1 = true) {
+        scale.animateTo(targetValue = 0.9f,
+        animationSpec = tween(durationMillis = 800,
+        easing = {
+            OvershootInterpolator(8f)
+                .getInterpolation(it)
+        }))
+        delay(2000)
+        navController.navigate(ReaderScreens.LoginScreen.name)
+    }
+
+    Surface(
+        modifier = Modifier
+            .padding(15.dp)
+            .size(330.dp)
+            .scale(scale.value),
+        shape = CircleShape,
+        border = BorderStroke(width = 2.dp, color = Color.LightGray)) {
+
+        Column(
+            modifier = Modifier.padding(1.dp),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ) {
+
+            Text(text = "Reader App",
+                fontSize = 40.sp,
+                color = Color.Red.copy(alpha = 0.5f))
+            Spacer(modifier = Modifier.height(15.dp))
+            Text(text = "\"Read. Change. Yourself\"",
+                fontSize = 20.sp,
+                color = Color.LightGray)
+
+        }
+
+    }
 }
--- ComposeReaderApp/app/src/main/java/com/onurdemir/composereaderapp/screens/login/ReaderLoginScreen.kt
+++ ComposeReaderApp/app/src/main/java/com/onurdemir/composereaderapp/screens/login/ReaderLoginScreen.kt
@@ -1,9 +1,10 @@
 package com.onurdemir.composereaderapp.screens.login
 
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.navigation.NavController
 
 @Composable
 fun ReaderLoginScreen(navController: NavController) {
-
+    Text(text = "LoginScreen")
 }
