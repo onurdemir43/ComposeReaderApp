69a6e345e5129dbb3d15368eadd83e7c729ffca3
onurdemir43 <142926729+onurdemir43@users.noreply.github.com>
2023-12-12 21:08:11 +0300

    Finishing up the Top Section

--- ComposeReaderApp/app/src/main/java/com/onurdemir/composereaderapp/screens/home/ReaderHomeScreen.kt
+++ ComposeReaderApp/app/src/main/java/com/onurdemir/composereaderapp/screens/home/ReaderHomeScreen.kt
@@ -1,10 +1,18 @@
 package com.onurdemir.composereaderapp.screens.home
 
 import android.annotation.SuppressLint
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccountCircle
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.Book
 import androidx.compose.material.icons.filled.Bookmark
@@ -12,6 +20,7 @@ import androidx.compose.material.icons.filled.BookmarkBorder
 import androidx.compose.material.icons.filled.Bookmarks
 import androidx.compose.material.icons.filled.LibraryBooks
 import androidx.compose.material.icons.filled.Logout
+import androidx.compose.material3.Divider
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
@@ -29,18 +38,25 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.draw.scale
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import com.google.firebase.auth.FirebaseAuth
+import com.onurdemir.composereaderapp.model.MBook
 import com.onurdemir.composereaderapp.navigation.ReaderScreens
 
+@Preview
 @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun Home(navController: NavController) {
+fun Home(navController: NavController = NavController(LocalContext.current)) {
     Scaffold(topBar = {
                       ReaderAppBar(title = "Reader App", navController = navController)
     },
@@ -52,6 +68,7 @@ fun Home(navController: NavController) {
         //content
         Surface(modifier = Modifier.fillMaxSize()) {
             //home content
+            HomeContent(navController)
         }
     }
 
@@ -71,14 +88,14 @@ fun ReaderAppBar(
                               Icon(
                                   imageVector = Icons.Default.LibraryBooks,
                                   contentDescription = "Logo Icon",
-                                  tint = Color.LightGray,
+                                  tint = Color(0xFF92CBDF).copy(alpha = 0.6f),
                                   modifier = Modifier
                                       .clip(RoundedCornerShape(12.dp))
                                       .scale(0.9f)
 
                               )
                           }
-                          Text(text = title, color = Color.Red.copy(alpha = 0.7f),
+                          Text(text = title, color = Color.Red.copy(alpha = 0.6f),
                           style = TextStyle(fontWeight = FontWeight.Bold, fontSize = 20.sp)
                           )
                       }
@@ -89,12 +106,67 @@ fun ReaderAppBar(
                           navController.navigate(ReaderScreens.LoginScreen.name)
                       }
                   }) {
-                      Icon(imageVector = Icons.Default.Logout, contentDescription = "Logout")
+                      Icon(imageVector = Icons.Default.Logout,
+                          contentDescription = "Logout",
+                          tint = Color(0xFF92CBDF).copy(alpha = 0.6f))
 
                   }
         },
-        colors = TopAppBarDefaults.smallTopAppBarColors(containerColor = Color(0xFFFFF8F0)))
+        colors = TopAppBarDefaults.smallTopAppBarColors(containerColor = Color.LightGray.copy(alpha = 0.1f)))
 
+
+}
+@Composable
+fun HomeContent(navController: NavController) {
+
+    val currentUser = if (!FirebaseAuth.getInstance().currentUser?.email.isNullOrEmpty()) {
+        FirebaseAuth.getInstance().currentUser?.email?.split('@')?.get(0)
+    }else {
+        "N/A"
+    }
+
+    Column(modifier = Modifier.padding(2.dp), verticalArrangement = Arrangement.SpaceEvenly) {
+        Row(modifier = Modifier.align(alignment = Alignment.Start)) {
+            TitleSection(label = "Your reading \n" + "activity right now...")
+
+            Spacer(modifier = Modifier.fillMaxWidth(0.7f))
+
+            Column {
+                Icon(imageVector = Icons.Default.AccountCircle,
+                    contentDescription = "Profile",
+                    tint = Color(0xFF92CBDF).copy(alpha = 0.6f),
+                modifier = Modifier
+                    .size(45.dp)
+                    .clickable {
+                        navController.navigate(ReaderScreens.ReaderStatsScreen.name)
+                    }
+                )
+                Text(text = currentUser!!,
+                modifier = Modifier.padding(2.dp),
+                color = Color.Red.copy(alpha = 0.6f),
+                fontSize = 15.sp,
+                maxLines = 1,
+                overflow = TextOverflow.Clip)
+                Divider()
+            }
+        }
+    }
+}
+
+@Composable
+fun TitleSection(modifier: Modifier = Modifier, label: String) {
+    Surface(modifier.padding(start = 4.dp, top = 2.dp)) {
+        Column {
+            Text(text = label,
+            fontSize = 20.sp,
+            fontStyle = FontStyle.Normal,
+            textAlign = TextAlign.Left)
+        }
+    }
+}
+
+@Composable
+fun ReadingBookRightNow(books: List<MBook>, navController: NavController) {
 
 }
 
--- ComposeReaderApp/app/src/main/java/com/onurdemir/composereaderapp/screens/stats/ReaderStatsScreen.kt
+++ ComposeReaderApp/app/src/main/java/com/onurdemir/composereaderapp/screens/stats/ReaderStatsScreen.kt
@@ -1,9 +1,10 @@
 package com.onurdemir.composereaderapp.screens.stats
 
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.navigation.NavController
 
 @Composable
 fun ReaderStatsScreen(navController: NavController) {
-
+    Text(text = "Stats Screen")
 }
